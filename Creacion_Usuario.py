4032e1f08937d636cc3105ff1ba5be2c940aa8e4
AlejandroSoPa <95918617+AlejandroSoPa@users.noreply.github.com>
2022-01-18 09:33:54 +0100

    Add files via upload

---
+++ Creacion_Usuario.py
@@ -0,0 +1,12 @@
+def usuario():
+    user = input("Username: ")
+    checkUser(user)
+
+
+def contraseña():
+    password = input("Password: ")
+    checkPassword(password)
+
+
+print(usuario())
+print(contraseña())
