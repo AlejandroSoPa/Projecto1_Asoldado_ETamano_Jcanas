1f8d24db86317d1a95bff4d7008e7f9315c4046a
JordiCanasDAM <95918677+JordiCanasDAM@users.noreply.github.com>
2022-01-19 08:13:13 +0100

    modificacions M2 casella next step i inserció de dades

--- M2/1_Create_DB.sql
+++ M2/1_Create_DB.sql
@@ -3,53 +3,88 @@ create database PROJECT_1;
 
 use PROJECT_1;
 
-create table USER (
- id_user Int,
+alter database PROJECT_1 character set utf8mb4;
+
+create table if not exists USER (
+ ID_USER Int,
  username varchar(20),
- password varchar(20)
+ password varchar(20),
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
  
- create table CHARACTERS (
- id_character int,
+ create table if not exists PROJECT_1.CHARACTER (
+ ID_CHARACTER int,
  name varchar(20),
- description varchar(150)
+ description varchar(750),
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
  
- create table ADVENTURE (
- id_adventure int,
+ create table if not exists ADVENTURE (
+ ID_ADVENTURE int,
  name varchar(20),
- description varchar(150)
+ description varchar(750),
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
  
- create table CHARACTER_ADVENTURE (
- id_character int,
- id_adventure int
+ create table if not exists CHARACTER_ADVENTURE (
+ ID_CHARACTER int,
+ ID_ADVENTURE int,
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
  
- create table STEP (
- id_step int,
- description varchar(150),
+ create table if not exists STEP (
+ ID_STEP int,
+ description varchar(750),
  adventure_end bit(1),
- id_adventure int
+ ID_ADVENTURE int,
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
  
- create table OPTIONS (
- id_option int,
- description varchar(150),
+ create table if not exists PROJECT_1.OPTION (
+ ID_OPTION int,
+ description varchar(750),
  answer varchar(50),
- id_step int
+ next_step int,
+ ID_STEP int,
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
 
- create table ROUND (
- id_round int,
+ create table if not exists ROUND (
+ ID_ROUND int,
  date datetime,
  time datetime,
- id_user int,
- id_characters int,
- id_adventure int
+ ID_USER int,
+ ID_CHARACTER int,
+ ID_ADVENTURE int,
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
  
- create table ROUND_OPTIONS (
- id_round int,
- id_options int
+ create table if not exists ROUND_OPTION (
+ ID_ROUND int,
+ ID_OPTION int,
+ usercreate varchar(20),
+ usermodify varchar(20),
+ datecreated datetime,
+ datemodified datetime
  );
--- M2/2_Alter_Table.sql
+++ M2/2_Alter_Table.sql
@@ -1,31 +1,70 @@
 use PROJECT_1;
 
 alter table USER
-modify id_user int primary key not null unique auto_increment,
+modify ID_USER int primary key not null unique auto_increment,
 modify username varchar(20) not null unique,
-modify password varchar(20) not null;
+modify password varchar(20) not null,
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime;
 
-alter table ROUND
-modify id_round int primary key not null unique auto_increment,
-modify date datetime not null unique,
-modify time datetime not null;
-
-alter table CHARACTERS
-modify id_character int primary key not null unique auto_increment,
+alter table PROJECT_1.CHARACTER
+modify ID_CHARACTER int primary key not null unique auto_increment,
 modify name varchar(20) not null unique,
-modify description varchar(150) not null unique;
+modify description varchar(750) not null unique,
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime;
 
 alter table ADVENTURE
-modify id_adventure int primary key not null unique auto_increment,
+modify ID_ADVENTURE int primary key not null unique auto_increment,
 modify name varchar(20) unique not null,
-modify description varchar(150) unique not null;
+modify description varchar(750) unique not null,
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime;
+
+alter table CHARACTER_ADVENTURE
+add constraint fk_CHARACTER_ADVENTURE_character foreign key (id_character) references PROJECT_1.CHARACTER(id_character),
+add constraint fk_CHARACTER_ADVENTURE_adventure foreign key (id_adventure) references ADVENTURE(id_adventure),
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime;
 
 alter table STEP
-modify id_step int primary key not null auto_increment,
-modify description varchar(150) not null,
-modify adventure_end bit(1) not null;
+modify ID_STEP int primary key not null auto_increment,
+modify description varchar(750) not null,
+modify adventure_end bit(1) not null,
+add constraint fk_step_adventure foreign key (id_adventure) references ADVENTURE(id_adventure),
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime;
 
-alter table OPTIONS
-modify id_option int primary key not null auto_increment,
-modify description varchar(150) not null,
-modify answer varchar(50) not null;
+alter table PROJECT_1.OPTION
+modify ID_OPTION int primary key not null auto_increment,
+modify description varchar(750) not null,
+modify answer varchar(50) not null,
+modify next_step int not null,
+add constraint fk_options_step foreign key (id_step) references STEP(id_step),
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime;
+
+alter table ROUND
+modify ID_ROUND int primary key not null unique auto_increment,
+modify date datetime not null unique,
+modify time datetime not null,
+add constraint fk_round_user foreign key (id_user) references USER(id_user),
+add constraint fk_round_character foreign key (id_character) references PROJECT_1.CHARACTER(id_character),
+add constraint fk_round_adventure foreign key (id_adventure) references ADVENTURE(id_adventure),
+modify usercreate varchar(20) not null,
+modify usermodify varchar(20),
+modify datecreated datetime not null,
+modify datemodified datetime
+;
