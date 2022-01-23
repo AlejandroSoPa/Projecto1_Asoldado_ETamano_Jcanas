d40fa7c9c666b7924992760a81afcfbfdbabbd26
eriktamano <95917206+eriktamano@users.noreply.github.com>
2022-01-23 15:40:04 +0100

    Delete Pruebas.py

--- M3/Pruebas.py
+++
@@ -1,3 +0,0 @@
-import funcions as f
-text = "Seguro que más de uno recuerda aquellos libros en los que podías elegir cómo seguir con la aventura que estabas viviendo simplemente"
-print(f.getFormatedBodyColumns((text, text, text), (20, 30, 50), 2))
